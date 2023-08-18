4255266bfa73111f6a7431d1e945d11d67fe57f2
yuanziqiang1995 <40654120+yuanziqiang1995@users.noreply.github.com>
2023-08-18 19:42:25 +0800

    update 818

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
                     output_path=params.formula_output_path,
                     max_variable_number=params.max_variable_number,
                     max_dsl_program_step=params.max_dsl_program_step)
-    '''
+
     generate(fun_path=params.formula_output_path,
              num_formula=params.generate_num_per_formula,
              begin_time=params.start_time,
@@ -21,7 +21,6 @@ if __name__ == '__main__':
              output_table_path=params.table_output_path,
              output_text_path=params.text_output_path,
              max_retry=params.max_retry,)
-    '''
     list_table = process_table(params.table_output_path, funs)
     list_text = process_text(params.text_output_path, funs)
 
